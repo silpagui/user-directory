b1076bcd81bd97808687207f80f047b804ad3a58
silpagui <silpagui.sa@gmail.com>
2022-01-16 21:40:31 +0100

    feat(UserCard): create UserCard component

--- src/components/HomePage/HomePage.component.js
+++ src/components/HomePage/HomePage.component.js
@@ -1,18 +1,11 @@
 import React from "react";
+import { UserCard } from "../UserCard/UserCard.component";
 import "./HomePage.styles.css";
 
 export function HomePage() {
 	return (
 		<div className="app-container">
-			<div className="user-card">
-				<div className="user-card__image">P</div>
-				<h2 className="user-card__name">Patricia Lebsack</h2>
-				<p className="user-card__username">@karianne</p>
-				<a href="https://kale.biz" className="user-card__website">
-					https://kale.biz
-				</a>
-				<button className="button">More Details</button>
-			</div>
+			<UserCard />
 		</div>
 	);
 }
---
+++ src/components/UserCard/UserCard.component.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./UserCard.styles.css";
+
+export function UserCard() {
+	return (
+		<div className="user-card">
+			<div className="user-card__image">P</div>
+			<h2 className="user-card__name">Patricia Lebsack</h2>
+			<p className="user-card__username">@karianne</p>
+			<a href="https://kale.biz" className="user-card__website">
+				https://kale.biz
+			</a>
+			<button className="button">More Details</button>
+		</div>
+	);
+}
