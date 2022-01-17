d625c756d8ec39836745aafdce6c1cac42e86eb0
silpagui <silpagui.sa@gmail.com>
2022-01-17 19:08:23 +0100

    feat(Button): implement Link from react-router-dom

--- src/components/Button/Button.component.js
+++ src/components/Button/Button.component.js
@@ -1,6 +1,12 @@
 import React from "react";
 import "./Button.styles.css";
 
-export function Button() {
-	return <button className="button">More Details</button>;
+import { Link } from "react-router-dom";
+
+export function Button(props) {
+	return (
+		<Link to={"/" + props.path} className="button">
+			More Details
+		</Link>
+	);
 }
