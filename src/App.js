49de33a96d4003de09fb3d1ec7b069c6e047b69d
silpagui <silpagui.sa@gmail.com>
2022-01-17 19:05:49 +0100

    feat(App): implement react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import { HomePage } from "./components/HomePage/HomePage.component";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./reset.css";
+import { UserDetails } from "./components/UserDetails/UserDetails.component";
 
 export function App() {
-	return <HomePage />;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route exact path="/" element={<HomePage />} />
+				<Route exact path="/:userId" element={<UserDetails />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
